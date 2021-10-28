0b8e38aed5abf80b3f4a2cbadf24185731e27686
Matheus Cerqueira <matheus.cerqueira1208@.com>
2021-10-28 10:15:05 -0300

    Subindo aplicação para o github

---
+++ src/components/SubscriptionButton/index.tsx
@@ -0,0 +1,41 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './style.module.scss';
+
+interface SubButtonProps {
+    priceId: string;
+}
+
+export function SubButton({priceId}:SubButtonProps){
+    const [session] = useSession()
+
+    async function handleSubscribe(){
+        if (!session) {
+            signIn('github')
+            return;
+        } 
+
+        try{
+            const response = await api.post('/subsSection')
+
+            const [sessionId] = response.data
+            
+            const stripe = await getStripeJs()
+
+            stripe.redirectToCheckout({sessionId})
+        } catch (err){
+            alert(err.message);
+        }
+    };
+
+    return(
+        <button 
+        type="button" 
+        className={styles.subButton}
+        onClick={handleSubscribe}
+        >
+            Subscribe now
+        </button>
+    )
+}
