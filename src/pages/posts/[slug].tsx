b3d598591b71a8c14212e918e9dd37c24a875361
Matheus Cerqueira <matheus.cerqueira1208@.com>
2021-11-03 12:10:23 -0300

    Return Post page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,64 +1,68 @@
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/client";
-import Head from 'next/head';
+import Head from "next/head";
 import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
 import styles from "./post.module.scss";
 
-
 interface PostProps {
-    post: {
-        slug: string,
-        title: string;
-        content: string;
-        updatedAt: string;
-    }
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
 }
 
-export default function Post({post}: PostProps) {
+export default function Post({ post }: PostProps) {
+  return (
     <>
-    <Head>
+      <Head>
         <title>{post.title} | Out of Context Brasa</title>
-    </Head>
+      </Head>
 
-    <main className={styles.container}>
+      <main className={styles.container}>
         <article className={styles.post}>
-
-            <h1 >{post.title}</h1>
-            <time className={styles.time}>{post.updatedAt}</time>
-            <div
+          <h1>{post.title}</h1>
+          <time className={styles.time}>{post.updatedAt}</time>
+          <div
             className={styles.postContent}
-            dangerouslySetInnerHTML={{__html: post.content }}
-            />
-
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
         </article>
-    </main>
-
+      </main>
     </>
+  );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({req, params}) =>{
-    const session = await getSession({req})
-    const {slug} = params;
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const session = await getSession({ req });
+  const { slug } = params;
 
-    const prismic = getPrismicClient(req)
+  const prismic = getPrismicClient(req);
 
-    const response = await prismic.getByUID('post', String(slug), {});
+  const response = await prismic.getByUID("pubs", String(slug), {});
 
-    const post = {
-        slug,
-        title: RichText.asText(response.data.title),
-        content: RichText.asHtml(response.data.content),
-        updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-        })
-    };
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
 
-    return{
-        props: {
-            post,
-        }
-    }
-}
+  return {
+    props: {
+      post,
+    },
+  };
+};
